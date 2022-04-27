3d47d60c1e23ca5a77d62438d959f0490819c885
Bill Cathey <billcathey@hotmail.com>
2022-04-26 23:22:00 -0400

    Setup webpack, create generateJoke module

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("I don't trust stairs. They're always up to something!")})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log(123)
+import generateJoke from './generateJoke'
+
+console.log(generateJoke())
