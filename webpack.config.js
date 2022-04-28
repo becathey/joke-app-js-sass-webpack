58f8247de775c6f855a78a302564ddcedd9b5aff
Bill Cathey <billcathey@hotmail.com>
2022-04-27 22:28:24 -0400

    Set up Html Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -16,5 +17,11 @@ module.exports = {
                 use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Joke App',
+            filename: 'index.html',
+        })
+    ]
 }
