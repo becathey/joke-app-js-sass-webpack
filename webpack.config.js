8194de5a92b3f0f3ba021115e359bf42e60f79d6
Bill Cathey <billcathey@hotmail.com>
2022-04-28 17:58:19 -0400

    Add clean property to output

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
+        clean: true,
     },
     module: {
         rules: [
