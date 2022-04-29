e72fb73fb9c568e0a48a855473d44b31442c315e
Bill Cathey <billcathey@hotmail.com>
2022-04-28 20:23:07 -0400

    Add event listener to button to get new joke

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ import laughing from './assets/laughing.svg'
 const image = document.getElementById('laughImg')
 laughImg.src = laughing
 
-console.log(generateJoke())
+const jokeBtn = document.getElementById('jokeBtn')
+jokeBtn.addEventListener('click', generateJoke)
+generateJoke()
